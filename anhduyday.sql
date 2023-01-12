a626907426f379fa947d08d59f528335bd025b97
cuongtn98 <cuonghoa1998@gmail.com>
2023-01-12 13:55:35 +0700

    commit

---
+++ anhduyday.sql
@@ -0,0 +1,4 @@
+select * from teacher;
+insert into teacher (id , Name , Age , country ) values (1,'cường',18 , 'VN' );
+insert into teacher (id , Name , Age , country ) values ( 2,'cường2', 17, 'vn');
+select * from teacher;
